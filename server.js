743ef613a368b9c1ad0ba48c5e5aa39e05532af1
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-11-12 20:28:45 +0600

    Saved user to db using googleLogin

--- server.js
+++ server.js
@@ -63,7 +63,15 @@ async function run() {
             const result = await usersCollection.insertOne(user)
             res.json(result)
         })
-      
+        //
+        app.put('/users', async(req,res)=>{
+            const user = req.body;
+            const filter = {email: user.email};
+            const options = { upsert : true};
+            const update = {$set : user}
+            const result =await usersCollection.updateOne(filter,update,options);
+            res.json(result)
+        })
     }
     finally {
         // await client.close();
